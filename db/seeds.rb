80ee1c0bf6d340cc72433d57a5227e815655339f
Muhidin <muhidinhukic1@gmail.com>
2020-11-23 16:28:56 -0600

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+song = Song.second
+song.update(artist_id: Artist.first.id)
